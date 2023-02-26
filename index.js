ff38762dcb57b23229c6701c18fc679256a71a00
nikifrei <124447598+nikifrei@users.noreply.github.com>
2023-02-26 11:13:23 +0100

    s

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const MH_Z19      = require('mh_z19');
 module.exports = function(homebridge){
   Service         = homebridge.hap.Service;
   Characteristic  = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-co2-sensor", "Co2Sensor", Co2SensorAccessory);
+  homebridge.registerAccessory("homebridge-co2-low-level", "Co2LowLevel", Co2SensorAccessory);
 }
 
 function Co2SensorAccessory(log, config) {
   this.log           = log;
   this.name          = config["name"];
   this.uart_path     = config["uart_path"];
-  this.schedule      = config["schedule"] || '*/5 * * * *';
-  this.warning_level = config["warning_level"] || 1500;
+  this.schedule      = config["schedule"] || '*/1 * * * *';
+  this.warning_level = config["warning_level"] || 800;
 
   this.informationService         = new Service.AccessoryInformation();
   this.CarbonDioxideSensorService = new Service.CarbonDioxideSensor(this.name);
@@ -29,9 +29,9 @@ function Co2SensorAccessory(log, config) {
             .updateCharacteristic(Characteristic.CarbonDioxideDetected, new Error(error));
         }
         else {
-          let co2_detected = (this.warning_level > co2_level) ? 0 : 1;
+          let co2_detected = (this.warning_level < co2_level) ? 0 : 1;
           this.log(`>>> [Update] CarbonDioxideLevel => ${co2_level}`);
-          this.log(`>>> [Update] CarbonDioxideDetected => ${co2_detected}`);
+          this.log(`>>> [Update] CarbonLowLevelDetected => ${co2_detected}`);
           this.CarbonDioxideSensorService
             .updateCharacteristic(Characteristic.CarbonDioxideLevel, co2_level);
           this.CarbonDioxideSensorService
