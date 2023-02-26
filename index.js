6ffc65914491d93b9379a76fb11e38fb06249eab
nikifrei <124447598+nikifrei@users.noreply.github.com>
2023-02-26 13:09:09 +0100

    n

--- index.js
+++ index.js
@@ -8,20 +8,19 @@ module.exports = function(homebridge){
   homebridge.registerAccessory("homebridge-co2-low-level", "CO2LowLevel", Co2SensorAccessory);
 }
 
-setTimeout(function() {
-  // Code, der erst nach 2 Sekunden ausgeführt wird
-  alert('Hello World!');
-}, 2000);
 
 function Co2SensorAccessory(log, config) {
   this.log           = log;
   this.name          = config["name"];
+  this.name2         = config["name2"]
   this.uart_path     = config["uart_path"];
   this.schedule      = config["schedule"] || '*/1 * * * *';
-  this.warning_level = config["warning_level"] || 800;
+  this.warning_level_high = config["warning_level_high"] || 1500;
+  this.warning_level_low = config["warning_level_low"] || 800;
 
   this.informationService         = new Service.AccessoryInformation();
   this.CarbonDioxideSensorService = new Service.CarbonDioxideSensor(this.name);
+  this.SmokeSensorService = new Service.SmokeSensor(this.name2);
 
   this.job = new CronJob({
     cronTime: this.schedule,
@@ -34,13 +33,17 @@ function Co2SensorAccessory(log, config) {
             .updateCharacteristic(Characteristic.CarbonDioxideDetected, new Error(error));
         }
         else {
-          let co2_detected = (this.warning_level < co2_level) ? 0 : 1;
+          let co2_high_detected = (this.warning_level_high < co2_level) ? 0 : 1;
+          let co2_low_detected = (this.warning_level_low > co2_level) ? 0 : 1;
           this.log(`>>> [Update] CarbonDioxideLevel => ${co2_level}`);
-          this.log(`>>> [Update] CarbonLowLevelDetected => ${co2_detected}`);
+          this.log(`>>> [Update] CarbonHighLevelDetected => ${co2_high_detected}`);
+          this.log(`>>> [Update] CarbonLowLevelDetected => ${co2_low_detected}`);
           this.CarbonDioxideSensorService
             .updateCharacteristic(Characteristic.CarbonDioxideLevel, co2_level);
           this.CarbonDioxideSensorService
-            .updateCharacteristic(Characteristic.CarbonDioxideDetected, co2_detected);
+            .updateCharacteristic(Characteristic.CarbonDioxideDetected, co2_high_detected);
+          this.SmokeSensorService
+            .updateCharacteristic(Characteristic.SmokeDetected, co2_low_detected)
         }
       }.bind(this))
     },
